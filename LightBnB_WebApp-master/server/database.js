5fbe1c2f0784597205ec260d7f583a539b533991
csaidane <cs.saidane@gmail.com>
2020-07-23 16:20:55 +0000

    reservation db connected

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -22,7 +22,6 @@ const pool = new Pool({
 
 const getUserWithEmail = function(email) {
   const queryString = `SELECT * FROM users WHERE email = $1`;
-
    return pool.query(queryString, [email])
   .then(res => res.rows[0]) 
 }
@@ -50,14 +49,6 @@ const addUser =  function(user) {
   const queryString = `INSERT INTO users(name,email,password) VALUES ($1, $2,$3) RETURNING *;`
   return pool.query(queryString, [user.name, user.email, user.password])
   .then(res => console.log(res))
-
-
-  /*
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-  */
 }
 exports.addUser = addUser;
 
@@ -68,8 +59,20 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+
+
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  queryString = `SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT 10;`;
+  return pool.query(queryString, [guest_id, limit])
+  .then(res => res.rows)
 }
 exports.getAllReservations = getAllReservations;
 
